e34f537e9fb1f9b7d79ff8c749fe24bd1f588d48
pogon96wiki <mietusekmintbot@gmail.com>
2026-09-04 13:25:37 +0200

    ..

---
+++ w/extensions/AnalyticsGoogle/main.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+const script = document.createElement("script");
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-EDJKV9Y1YP";
+document.head.appendChild(script);
+
+gtag("js", new Date());
+gtag("config", "G-EDJKV9Y1YP");
